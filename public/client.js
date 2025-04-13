21814e37ddb50f2bdde0a1a03500f5748ce4ee28
jcmorillomzn <jcmorillomzn@gmail.com>
2025-04-14 00:07:59 +0200

    añadido panel.html y botón del Power-Up

--- public/client.js
+++ public/client.js
@@ -5,13 +5,12 @@ var Promise = TrelloPowerUp.Promise;
 TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
-      icon: 'https://cdn-icons-png.flaticon.com/512/929/929564.png',
       text: '👑 Panel JC',
       callback: function (t) {
         return t.popup({
-          title: 'Cargando Panel JC...',
+          title: 'Panel JC',
           url: 'panel.html',
-          height: 400
+          height: 600
         });
       }
     }];
